74ae676bad8a58ac804f35c4909ee7f940a17a86
Varundeep06 <139569049+Varundeep06@users.noreply.github.com>
2024-05-02 10:29:00 -0500

    upoloaded the files to the github

---
+++ sandbox.js
@@ -0,0 +1,55 @@
+// // alert("Are u 18+");
+
+// let age = 25;
+// let year = 2019
+// console.log(age,year)
+//  age =100 
+//  console.log(age)
+// // we cannot chnge or overwrite the value of the const
+//  const points = 200;
+//  console.log(points)
+// // points = 26600;
+
+// var score = 77;
+// console.log(score)
+
+// // strings 
+// console.log("HEllo THIS IS VARUNDEEP SINGH");
+// let email ='varunsohal18@gmail.com';
+// console.log(email)
+
+// // String concentenation
+// let firstname = "varundeep";
+// let lastname = 'singh';
+// let fullname = firstname+' '+lastname;
+// console.log(fullname)
+
+// // getting character
+// console.log(fullname[0]);
+
+// // string strength 
+// console.log(fullname.length)
+
+// // String methods 
+// console.log(fullname.toUpperCase())
+// // let result = fullname.toLowerCase();
+// // console.log(result, fullname);
+// // this is also to count the character until the written character
+// let index =email.indexOf('@');
+// console.log(index)
+
+// // common string method
+// let Email = 'varunsohal18@gmail.com';
+// // let result = email.lastIndexOf('r');
+// // let result = Email.slice(0,10);
+// // let result = Email.substr(4,10);
+// // let result =Email.replace('v','b')
+// console.log(result);
+
+let radius = 11;
+const pi =3.12;
+
+// console.log(radius, pi);
+// math opreators 
+// console.log(10/2)
+
